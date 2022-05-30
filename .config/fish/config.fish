60cfd424be5f1ee1f76cb7809288b9907114dd81
Justin Guerré <guerrejustin@gmail.com>
2022-05-30 18:45:11 +0200

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,40 @@
+set fish_greeting
+starship init fish | source
+set PATH /home/justin/.npm-global/bin $PATH
+set PATH /home/justin/.yarn/bin $PATH
+# set TERM screen-256color
+
+# go
+set PATH /usr/local/go/bin $PATH
+set PATH /home/justin/go/bin $PATH
+set GOBIN /home/justin/go/bin/
+set GOPATH /home/justin/go
+
+set PATH $HOME/Android/android-studio/bin $PATH
+set ANDROID_HOME $HOME/Android/Sdk
+set PATH $ANDROID_HOME/emulator $PATH
+set PATH $ANDROID_HOME/tools $PATH
+set PATH $ANDROID_HOME/tools/bin $PATH
+set PATH $ANDROID_HOME/platform-tools $PATH
+
+# set PATH /home/justin/.platformio/penv/bin $PATH
+set PATH $HOME/.cargo/env $PATH
+
+alias ssh="kitty +kitten ssh"
+alias ls="exa -l"
+# alias cat="bat"
+alias python=python3
+alias pip=pip3
+alias pio=platformio
+
+function cheat.sh
+    curl cheat.sh/$argv
+end
+
+set FISH_CONFIG ~/.config/fish/config.fish
+
+alias tmux='tmux -2'
+alias lg='lazygit'
+alias icat='kitty +kitten icat'
+alias config='/usr/bin/git --git-dir=/home/justin/.cfg/ --work-tree=/home/justin'
+alias lgc='lazygit --git-dir=$HOME/.cfg --work-tree=$HOME'
