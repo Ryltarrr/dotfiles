5207d042fedff5b2f0a7b9305de32ec263cc6216
Justin Guerré <guerrejustin@gmail.com>
2022-08-09 20:29:43 +0200

    replace with default formatters

--- .config/nvim/after/plugin/formatter.lua
+++ .config/nvim/after/plugin/formatter.lua
@@ -1,121 +1,47 @@
 local group_id = vim.api.nvim_create_augroup("FormatAutogroup", {})
 vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = "*",
-  command = "FormatWrite",
-  group = group_id
+	pattern = "*",
+	command = "FormatWrite",
+	group = group_id,
 })
 
-require('formatter').setup({
-  filetype = {
-    vue = {
-      -- prettier
-      function()
-        return {
-          exe = "prettier",
-          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-          stdin = true
-        }
-      end
-    },
-    javascript = {
-      -- prettier
-      function()
-        return {
-          exe = "prettier",
-          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-          stdin = true
-        }
-      end
-    },
-    typescript = {
-      -- prettier
-      function()
-        return {
-          exe = "prettier",
-          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-          stdin = true
-        }
-      end
-    },
-    javascriptreact = {
-      -- prettier
-      function()
-        return {
-          exe = "prettier",
-          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-          stdin = true
-        }
-      end
-    },
-    typescriptreact = {
-      -- prettier
-      function()
-        return {
-          exe = "prettier",
-          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-          stdin = true
-        }
-      end
-    },
-    html = {
-      -- prettier
-      function()
-        return {
-          exe = "prettier",
-          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-          stdin = true
-        }
-      end
-    },
-    dart = {
-      function()
-        return {
-          exe = 'flutter format',
-          args = {
-            vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
-          },
-          stdin = false
-        }
-      end
-    },
-    python = {
-      function()
-        return {
-          exe = 'python3 -m autopep8',
-          args = {
-            '--in-place --aggressive --aggressive',
-            vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
-          },
-          stdin = false
-        }
-      end
-    },
-    rust = {
-      function()
-        return {
-          exe = "rustfmt",
-          args = {"--emit=stdout"},
-          stdin = true
-        }
-      end
-    },
-    go = {
-      function()
-        return {
-          exe = "gofmt",
-          stdin = true
-        }
-      end
-    },
-    prisma = {
-      -- prettier
-      function()
-        return {
-          exe = "prettier",
-          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
-          stdin = true
-        }
-      end
-    },
-  },
+require("formatter").setup({
+	filetype = {
+		vue = {
+			require("formatter.defaults.prettier"),
+		},
+		javascript = {
+			require("formatter.defaults.prettier"),
+		},
+		typescript = {
+			require("formatter.defaults.prettier"),
+		},
+		javascriptreact = {
+			require("formatter.defaults.prettier"),
+		},
+		typescriptreact = {
+			require("formatter.defaults.prettier"),
+		},
+		html = {
+			require("formatter.defaults.prettier"),
+		},
+		python = {
+			require("formatter.filetypes.python").autopep8,
+		},
+		rust = {
+			require("formatter.filetypes.rust").rustfmt,
+		},
+		go = {
+			require("formatter.filetypes.go").gofmt,
+		},
+		prisma = {
+			require("formatter.defaults.prettier"),
+		},
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+		["*"] = {
+			require("formatter.filetypes.any").remove_trailing_whitespace,
+		},
+	},
 })
