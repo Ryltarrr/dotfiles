71ceed181a3e6bb1e844d0cf7fd4864364f3e91d
jug <justin.guerre@anyti.me>
2025-01-23 09:13:00 +0100

    Add snacks + remove old lazygit

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,25 +1,26 @@
 return {
-    {
-        'stevearc/conform.nvim',
-        opts = {
-            formatters_by_ft = {
-                lua = { "stylua" },
-                -- Conform will run multiple formatters sequentially
-                python = { "isort", "black" },
-                -- You can customize some of the format options for the filetype (:help conform.format)
-                rust = { "rustfmt", lsp_format = "fallback" },
-                -- Conform will run the first available formatter
-                javascript = { "prettierd", "prettier", stop_after_first = true },
-                typescript = { "prettierd", "prettier", stop_after_first = true },
-                svelte = { "prettierd", "prettier", stop_after_first = true },
-                jsx = { "prettierd", "prettier", stop_after_first = true },
-                tsx = { "prettierd", "prettier", stop_after_first = true },
-            },
-            format_on_save = {
-                -- I recommend these options. See :help conform.format for details.
-                lsp_format = "fallback",
-                timeout_ms = 500,
-            },
-        },
-    }
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "isort", "black" },
+				-- You can customize some of the format options for the filetype (:help conform.format)
+				rust = { "rustfmt", lsp_format = "fallback" },
+				-- Conform will run the first available formatter
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				svelte = { "prettierd", "prettier", stop_after_first = true },
+				jsx = { "prettierd", "prettier", stop_after_first = true },
+				tsx = { "prettierd", "prettier", stop_after_first = true },
+				php = { nil },
+			},
+			format_on_save = {
+				-- I recommend these options. See :help conform.format for details.
+				lsp_format = "fallback",
+				timeout_ms = 500,
+			},
+		},
+	},
 }
